0462a219323f579026eaa31b1bffaf728e5448ce
annasun919 <yuval_sun@yahoo.com.hk>
2021-07-02 14:20:23 +0200

    add main and set up login

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,6 @@ const userSchema = new Schema({
   username: {  
     type: String,
     unique: true,
-<<<<<<< HEAD
     required: true,
   },
 
@@ -13,19 +12,6 @@ const userSchema = new Schema({
     type: String,
     required: true,
   }  
-=======
-    require: true,
-  },
-  password: {
-    type: String,
-    required : true
- 
-  },
-  favoritefish:{
-    type : String,
-    required : true
-  }
->>>>>>> f32b62cab81abaf9ceab49c4a22004d0cc0f776d
 });
 
 const User = model("User", userSchema);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+let bcrypt = require("bcryptjs");
+let UserModel = require("../models/User.model");
 
 /* GET home page */
 router.get("/login", (req, res, next) => {
@@ -6,15 +8,13 @@ router.get("/login", (req, res, next) => {
 });
 
 router.get("/signup", (req, res, next) => {
-    res.render("./auth/signup.hbs");
-  });
-
-module.exports = router;
+  res.render("./auth/signup.hbs");
+});
 
 router.post("/signup", (req, res, next) => {
-  const { username, password } = req.body;
+  const { username, password, favoritefish } = req.body;
 
-  if (!username || !password) {
+  if (!username || !password || !favoritefish) {
     res.render("auth/signup.hbs", { error: "Please enter all fields" });
     //to tell JS to come our off this function
     return;
@@ -23,8 +23,9 @@ router.post("/signup", (req, res, next) => {
   let regularExpression =
     /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
   if (!regularExpression.test(password)) {
-    res.render("auth/signup.hbs", {
-      error: "special charater , a number  and 6-16 characters",
+    res.render("./auth/signup.hbs", {
+      error:
+        "Invalid password, the valid password should have 6-16 characters and with at least one speail character",
     });
     // To tell JS to come out off this function
     return;
@@ -33,9 +34,30 @@ router.post("/signup", (req, res, next) => {
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
 
-  UserModel.create({ username, email, password: hash })
+  UserModel.create({ username, password: hash, favoritefish })
     .then(() => res.redirect("/"))
     .catch((err) => {
       next(err);
     });
 });
+
+router.post("/login", (req, res, next) => {
+  const { username, password, favoritefish } = req.body;
+
+  UserModel.findOne({ username })
+    .then((user) => {
+      let isValide = bcrypt.compareSync(password, user.password);
+      if (isValide) {
+        res.redirect("/profile");
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/profile", (req, res, next) => {
+  res.render("./auth/profile.hbs");
+});
+
+module.exports = router;
