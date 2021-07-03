b1bb204800a628aac446ccda4bec368966eb7489
annasun919 <yuval_sun@yahoo.com.hk>
2021-07-03 13:08:37 +0200

    create fish page

--- routes/fish.routes.js
+++ routes/fish.routes.js
@@ -6,27 +6,35 @@ router.get("/fish", (req, res, next) => {
   const json = JSON.stringify(req.query);
   console.log(`hihi  ${json}`);
   axios
-    .get(`https://www.fishwatch.gov/api/species/${req.query.species}`)
+    .get(`https://www.fishwatch.gov/api/species`)
     .then((response) => {
       let listoffish = response.data;
-      console.log(listoffish[0]["Species Illustration Photo"].src);
+      let fishphoto = listoffish[0]["Species Illustration Photo"].src;
 
-      FishModel.create({
-        habitat: listoffish[0].Habitat,
-        habitatImpacts: listoffish[0]["Habitat Impacts"],
-        location: listoffish[0].Location,
-        population: listoffish[0].Population,
-        scientificName: listoffish[0]["Scientific Name"],
-        speciesIllustrationPhoto:
-          listoffish[0]["Species Illustration Photo"].src,
-        speciesName: listoffish[0]["Species Name"],
-        biology: listoffish[0].Biology,
-      });
+      res.render("main/fish.hbs", { fishphoto });
+
+      // FishModel.create({
+      //   habitat: listoffish[0].Habitat,
+      //   habitatImpacts: listoffish[0]["Habitat Impacts"],
+      //   location: listoffish[0].Location,
+      //   population: listoffish[0].Population,
+      //   scientificName: listoffish[0]["Scientific Name"],
+      //   speciesIllustrationPhoto:
+      //     listoffish[0]["Species Illustration Photo"].src,
+      //   speciesName: listoffish[0]["Species Name"],
+      //   biology: listoffish[0].Biology,
+      // });
     })
     .catch((err) => {
       console.log(err);
-      next("Mars api fails");
+      next("fish api fails");
     });
+});
+
+// ${req.query.species}
+
+router.get("/profile", (req, res, next) => {
+  res.render;
 });
 
 module.exports = router;
